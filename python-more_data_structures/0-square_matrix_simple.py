3d2d0066b1e70aac4499de2943c89923fd7a9bfd
RosarioGle <8824@holbertonstudents.com>
2025-01-21 06:43:08 +0000

    creation du fichier 0-square_matrix_simple.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for elems in matrix[:]:
+        new_matrix.append(list(map(lambda x: x ** 2, elems)))
+    return new_matrix
