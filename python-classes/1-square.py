2173f78eac1249136aeb24881d020b4ae5149eab
RosarioGle <8824@holbertonstudents.com>
2025-01-29 12:24:48 +0100

    création du fichier 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""create an empty class"""
+
+
+class Square:
+    """define the class square"""
+    def __init__(self, size):
+        """Initialize the new Square
+
+        Args:
+
+        size: the size
+        """
+        self.__size = size
