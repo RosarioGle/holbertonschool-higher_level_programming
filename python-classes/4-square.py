e94e9a5373236a941e7b95ebd6af571003168fb7
RosarioGle <8824@holbertonstudents.com>
2025-01-29 13:02:05 +0100

    création du fichier 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""create an empty class"""
+
+
+class Square:
+    """define the class square"""
+    def __init__(self, size):
+        """Initialize the new Square
+
+        Args:
+
+        size: the size
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """The size od the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """The value is the square length"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the square area"""
+        return self.__size * self.__size
