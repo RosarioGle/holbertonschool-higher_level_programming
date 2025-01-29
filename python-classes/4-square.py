9911dd1a9e3239b747b89a06c129cc59e73f0e8b
RosarioGle <8824@holbertonstudents.com>
2025-01-29 13:34:02 +0100

    troisème correction du fichier 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,28 +4,23 @@
 
 
 class Square:
-    """define the class square"""
-    def __init__(self, size):
-        """Initialize the new Square
+    """Define the classe square."""
+    def __init__(self, size=0):
+        """Initialize the new Square.
 
         Args:
-
         size: the size
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
     def size(self):
-        """The size od the square"""
+        """the size of the square"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        """The value is the square length"""
+        """the value is the square length"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -34,4 +29,4 @@ class Square:
 
     def area(self):
         """Return the square area"""
-        return self.__size * self.__size
+        return (self.__size * self.__size)
