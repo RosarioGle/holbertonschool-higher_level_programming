c3daf776cbe5efdc250beca327eded3e67ea4e0d
RosarioGle <8824@holbertonstudents.com>
2025-01-29 12:53:06 +0100

    correction des erreurs de sorties dans le fichier 2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -13,7 +13,7 @@ class Square:
         size: the size
         """
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
