99f225c225e82d052da8574c7052613055abe613
RosarioGle <8824@holbertonstudents.com>
2025-01-30 13:53:19 +0100

    correction d'une erreur de sortie dans le fichier 1-rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,6 +27,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
