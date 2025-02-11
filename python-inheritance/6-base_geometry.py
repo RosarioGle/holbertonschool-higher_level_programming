0349bbc92e3247efc2038e99bc3dee9531cfe419
RosarioGle <8824@holbertonstudents.com>
2025-02-11 16:05:50 +0100

    création du fichier 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """the class BaseGeometry"""
+    def area(self):
+        """raises an Exception with the
+        message area() is not implemented"""
+        raise Exception("area() is not implemented")
