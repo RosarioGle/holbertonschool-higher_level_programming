0ee82f05316879a86d363f3b2e5bcefbd8154ea8
RosarioGle <8824@holbertonstudents.com>
2025-02-13 15:32:49 +0100

    création des fichiers 0-read_file.py et my_file_0.txt

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+reads a text file (UTF8) and prints it to stdout
+"""
+
+def read_file(filename=""):
+    """
+    reads and print the content of a text file
+    
+    Args:
+        filename (str): the name/path of the file
+    """
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
