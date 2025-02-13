49db40728eb960fc880e7f5e0e00c908bb9fe6bf
RosarioGle <8824@holbertonstudents.com>
2025-02-13 15:04:39 +0100

    légère correction du fichier 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,16 +11,23 @@ class Rectangle(BaseGeometry):
         """Initialize a new rectangle.
         Args:
             width (int): width of the rectangle
-            height (int): height of the rectangle"""
+            height (int): height of the rectangle
+        Raises:
+            TypeError: If width or height is not an integer
+            ValueError: If width or height is less than or equal to 0
+        """
         self.integer_validator("width", width)
-        self.__width = width
         self.integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
-        """Return the square area"""
+        """
+        Returns:
+            int: the area of the rectangle
+        """
         return self.__width * self.__height
 
     def __str__(self):
         """print Rectangle with the width and height"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
