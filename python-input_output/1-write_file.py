3d1b7295ca8a59377c131cc2238784c37180d736
RosarioGle <8824@holbertonstudents.com>
2025-02-13 15:50:46 +0100

    création du fichier 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/nin/python3
+"""
+writes a string to a text file (UTF8) and returns the
+number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string to a text file (UTF8) and returns the number of characters written
+
+    Args:
+        filename (str): the name/path of the file to write
+        text (str): the string to write to the file
+
+    Returns:
+        int: the number of characters written to the file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
