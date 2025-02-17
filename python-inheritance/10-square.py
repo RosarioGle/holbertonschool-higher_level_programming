b155495f6bbd9391e068c70638e4e81e5ac5885d
RosarioGle <8824@holbertonstudents.com>
2025-02-17 15:23:56 +0100

    modification du code du fichier 10-square.py

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -5,13 +5,20 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Define a Square."""
+    """Define a Square"""
 
     def __init__(self, size):
         """Initialize a new square.
 
         Args:
             size (int): size of the square"""
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
+        self.__size = self.integer_validator("size", size)
+
+    def area(self):
+        """
+        area of the square
+
+        Returns:
+            int: square of the size
+        """
+        return self.__size ** 2
