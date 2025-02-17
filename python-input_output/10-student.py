d2b21fed923caf2e246eb08bb84dfda9908a6618
RosarioGle <8824@holbertonstudents.com>
2025-02-17 09:55:05 +0100

    création du fichier 10-main.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+define a student class
+"""
+
+
+class Student:
+    """
+    the student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Args:
+            first_name (str): the first name of the student
+            last_name (str): the last name of the sutdent
+            age (int): the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Args:
+            attrs (list, optional): List of attribute names
+
+        Returns:
+            dict: a dictionary containing the student's attributes
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
