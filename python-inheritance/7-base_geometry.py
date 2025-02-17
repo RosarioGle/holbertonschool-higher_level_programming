7d19fb50e1f009da2ce978056d98804393af64cc
RosarioGle <8824@holbertonstudents.com>
2025-02-17 14:43:08 +0100

    1230

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,23 +4,23 @@
 
 class BaseGeometry:
     """the class BaseGeometry"""
+
     def area(self):
-        """raises an Exception with the
-        message area() is not implemented"""
+        """raises an Exception with the message
+        area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Verify if the value is an integer and superior to 0
 
         Args:
-            name(str): name of the form
-            value(int): the value
+            name (str): name of the form
+            value (int): value
 
         Raises:
             TypeError: <name> must be an integer
             ValueError: <name> must be greater than 0"""
-        if not isinstance(value, int) or isinstance(value, bool):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        return value
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""
-Write a class Rectangle who inherit
-from BaseGeometry (7-base_geometry.py)
-"""
+"""Write a class Rectangle who inherit
+from BaseGeometry (7-base_geometry.py)"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    Define a Rectangle
-    """
+    """Define a Rectangle."""
 
     def __init__(self, width, height):
-        """
-        Instantiation with width and height
-        Args:
-            width (int): the width of the rectangle
-            height (int): the height of the rectangle
-
-        Raises:
-            TypeError: If width or height is not an integer
-            ValueError: If width or height is less than or equal to 0
-        """
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
+        """Instantiation with width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
