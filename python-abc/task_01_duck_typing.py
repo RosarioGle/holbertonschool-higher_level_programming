4aa9fbd7000cca4958c1265043467d185324aa88
RosarioGle <8824@holbertonstudents.com>
2025-02-18 13:31:51 +0100

    création du fichier task_01_duck_typing.py

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""
+class named Shape with two abstract methods: area and perimeter
+"""
+from abc import *
+
+
+class Shape(ABC):
+    """
+    define Shape class
+    """
+    @abstractmethod
+    def area(self):
+        """
+        abstract base class representing a geometric shape
+        """
+        pass
+
+class Circle(Shape):
+    """
+    define Circle class
+    """
+    def __init__(self, radius):
+        """
+        initialize Circle with a radius
+        """
+        self.radius = radius
+
+    def area(self):
+        """
+        calculate and return the area of the circle
+        """
+        return 3.14159 * self.radius ** 2
+
+    def perimeter(self):
+        """
+        calculate and return the perimeter of the circle
+        """
+        return 2 * 3.14159 * self.radius
+
+class Rectangle(Shape):
+    """
+    define Rectangle class
+    """
+    def __init__(self, width, height):
+        """
+        initialize Rectangle with width and height
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """
+        calculate and return the area of the rectangle
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        calculate and return the parimeter of the rectangle
+        """
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    """
+    print the area and the perimeter
+    """
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
