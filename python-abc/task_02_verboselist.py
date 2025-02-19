61eb576fa42ed4015cb7e6ffcee4f3c8183889f0
RosarioGle <8824@holbertonstudents.com>
2025-02-19 12:26:45 +0100

    ajout de commentaire dans le fichier task_02_verboselist.py

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -9,15 +9,24 @@ class VerboseList(list):
     define VerboseList class
     """
     def append(self, item):
+        """
+        Adds an item to the end of the list
+        """
         super().append(item)
         print("Added [{}] to the list.".format(item))
 
     def extend(self, iterable):
+        """
+        Adds several items to the list in a single operation
+        """
         count = len(iterable)
         super().extend(iterable)
         print("Extended the list with [{}] items.".format(count))
 
     def remove(self, item):
+        """
+        Deletes a specific item from the list
+        """
         if item in self:
             print("Removed [{}] from the list.".format(item))
             super().remove(item)
@@ -25,6 +34,10 @@ class VerboseList(list):
             print("Item [{}] not found in the list.".format(item))
 
     def pop(self, index=-1):
+        """
+        Deletes and returns an element to a given position
+        (by default, the last one)
+        """
         if self:
             item = self[index]
             print("Popped [{}] from the list.".format(item))
